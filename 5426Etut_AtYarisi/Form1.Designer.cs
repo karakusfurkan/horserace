c269b779cc97b4332b5a2aa080641c96ff3d33e6
karakusfurkan <karakus.afurkan@gmail.com>
2019-05-06 01:41:58 +0300

    Veli Efendi

---
+++ 5426Etut_AtYarisi/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace _5426Etut_AtYarisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnYarisiBaslat = new System.Windows.Forms.Button();
+            this.btnYarisiSifirla = new System.Windows.Forms.Button();
+            this.pcbUcuncuAt = new System.Windows.Forms.PictureBox();
+            this.pcbIkinciAt = new System.Windows.Forms.PictureBox();
+            this.pcbBirinciAt = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblFinish = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnRuzgarGaz = new System.Windows.Forms.Button();
+            this.btnKaraSimsekGaz = new System.Windows.Forms.Button();
+            this.btnSahbaturGaz = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbUcuncuAt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbIkinciAt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbBirinciAt)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnYarisiBaslat
+            // 
+            this.btnYarisiBaslat.Location = new System.Drawing.Point(12, 12);
+            this.btnYarisiBaslat.Name = "btnYarisiBaslat";
+            this.btnYarisiBaslat.Size = new System.Drawing.Size(151, 32);
+            this.btnYarisiBaslat.TabIndex = 0;
+            this.btnYarisiBaslat.Text = "YARIŞI BAŞLAT";
+            this.btnYarisiBaslat.UseVisualStyleBackColor = false;
+            this.btnYarisiBaslat.Click += new System.EventHandler(this.btnYarisiBaslat_Click);
+            // 
+            // btnYarisiSifirla
+            // 
+            this.btnYarisiSifirla.Location = new System.Drawing.Point(169, 12);
+            this.btnYarisiSifirla.Name = "btnYarisiSifirla";
+            this.btnYarisiSifirla.Size = new System.Drawing.Size(151, 32);
+            this.btnYarisiSifirla.TabIndex = 0;
+            this.btnYarisiSifirla.Text = "YARIŞI SIFIRLA";
+            this.btnYarisiSifirla.UseVisualStyleBackColor = false;
+            this.btnYarisiSifirla.Click += new System.EventHandler(this.btnYarisiSifirla_Click);
+            // 
+            // pcbUcuncuAt
+            // 
+            this.pcbUcuncuAt.Image = global::_5426Etut_AtYarisi.Properties.Resources.Horse3;
+            this.pcbUcuncuAt.Location = new System.Drawing.Point(12, 332);
+            this.pcbUcuncuAt.Name = "pcbUcuncuAt";
+            this.pcbUcuncuAt.Size = new System.Drawing.Size(90, 106);
+            this.pcbUcuncuAt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbUcuncuAt.TabIndex = 1;
+            this.pcbUcuncuAt.TabStop = false;
+            // 
+            // pcbIkinciAt
+            // 
+            this.pcbIkinciAt.Image = global::_5426Etut_AtYarisi.Properties.Resources.Horse2;
+            this.pcbIkinciAt.Location = new System.Drawing.Point(12, 196);
+            this.pcbIkinciAt.Name = "pcbIkinciAt";
+            this.pcbIkinciAt.Size = new System.Drawing.Size(90, 106);
+            this.pcbIkinciAt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbIkinciAt.TabIndex = 1;
+            this.pcbIkinciAt.TabStop = false;
+            // 
+            // pcbBirinciAt
+            // 
+            this.pcbBirinciAt.Image = global::_5426Etut_AtYarisi.Properties.Resources.Horse1;
+            this.pcbBirinciAt.Location = new System.Drawing.Point(12, 59);
+            this.pcbBirinciAt.Name = "pcbBirinciAt";
+            this.pcbBirinciAt.Size = new System.Drawing.Size(90, 106);
+            this.pcbBirinciAt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pcbBirinciAt.TabIndex = 1;
+            this.pcbBirinciAt.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(-1, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(462, 13);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(-1, 310);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(462, 13);
+            this.label2.TabIndex = 2;
+            // 
+            // lblFinish
+            // 
+            this.lblFinish.BackColor = System.Drawing.Color.White;
+            this.lblFinish.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFinish.Location = new System.Drawing.Point(458, -2);
+            this.lblFinish.Name = "lblFinish";
+            this.lblFinish.Size = new System.Drawing.Size(15, 465);
+            this.lblFinish.TabIndex = 2;
+            this.lblFinish.Text = "FINISH";
+            this.lblFinish.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnRuzgarGaz
+            // 
+            this.btnRuzgarGaz.BackColor = System.Drawing.Color.Tan;
+            this.btnRuzgarGaz.Location = new System.Drawing.Point(487, 87);
+            this.btnRuzgarGaz.Name = "btnRuzgarGaz";
+            this.btnRuzgarGaz.Size = new System.Drawing.Size(112, 51);
+            this.btnRuzgarGaz.TabIndex = 3;
+            this.btnRuzgarGaz.Text = "RÜZGAR GAZ";
+            this.btnRuzgarGaz.UseVisualStyleBackColor = false;
+            this.btnRuzgarGaz.Click += new System.EventHandler(this.btnRuzgarGaz_Click);
+            // 
+            // btnKaraSimsekGaz
+            // 
+            this.btnKaraSimsekGaz.BackColor = System.Drawing.Color.Tan;
+            this.btnKaraSimsekGaz.Location = new System.Drawing.Point(487, 214);
+            this.btnKaraSimsekGaz.Name = "btnKaraSimsekGaz";
+            this.btnKaraSimsekGaz.Size = new System.Drawing.Size(112, 51);
+            this.btnKaraSimsekGaz.TabIndex = 3;
+            this.btnKaraSimsekGaz.Text = "KARA ŞİMŞEK GAZ";
+            this.btnKaraSimsekGaz.UseVisualStyleBackColor = false;
+            this.btnKaraSimsekGaz.Click += new System.EventHandler(this.btnKaraSimsekGaz_Click);
+            // 
+            // btnSahbaturGaz
+            // 
+            this.btnSahbaturGaz.BackColor = System.Drawing.Color.Tan;
+            this.btnSahbaturGaz.Location = new System.Drawing.Point(487, 352);
+            this.btnSahbaturGaz.Name = "btnSahbaturGaz";
+            this.btnSahbaturGaz.Size = new System.Drawing.Size(112, 51);
+            this.btnSahbaturGaz.TabIndex = 3;
+            this.btnSahbaturGaz.Text = "ŞAHBATUR GAZ";
+            this.btnSahbaturGaz.UseVisualStyleBackColor = false;
+            this.btnSahbaturGaz.Click += new System.EventHandler(this.btnSahbaturGaz_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(615, 457);
+            this.Controls.Add(this.btnSahbaturGaz);
+            this.Controls.Add(this.btnKaraSimsekGaz);
+            this.Controls.Add(this.btnRuzgarGaz);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblFinish);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pcbUcuncuAt);
+            this.Controls.Add(this.pcbIkinciAt);
+            this.Controls.Add(this.pcbBirinciAt);
+            this.Controls.Add(this.btnYarisiSifirla);
+            this.Controls.Add(this.btnYarisiBaslat);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pcbUcuncuAt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbIkinciAt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbBirinciAt)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnYarisiBaslat;
+        private System.Windows.Forms.Button btnYarisiSifirla;
+        private System.Windows.Forms.PictureBox pcbBirinciAt;
+        private System.Windows.Forms.PictureBox pcbIkinciAt;
+        private System.Windows.Forms.PictureBox pcbUcuncuAt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblFinish;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnRuzgarGaz;
+        private System.Windows.Forms.Button btnKaraSimsekGaz;
+        private System.Windows.Forms.Button btnSahbaturGaz;
+    }
+}
+
---
+++ 5426Etut_AtYarisi/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5426Etut_AtYarisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnYarisiBaslat_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)//timer'ım çalışıyorsa
+            {
+                timer1.Stop();//durdur
+            }
+            else//timer'ım çalışmıyorsa
+            {
+                timer1.Start();//başlat
+            }
+        }
+        Random rnd = new Random();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pcbBirinciAt.Left += rnd.Next(5,16);
+            pcbIkinciAt.Left += rnd.Next(5,16);
+            pcbUcuncuAt.Left += rnd.Next(5,16);
+
+            if (pcbBirinciAt.Left > pcbIkinciAt.Left && pcbBirinciAt.Left > pcbUcuncuAt.Left)
+            {
+                this.Text = "Rüzgar önde götürüyor...";
+            }
+            else if (pcbIkinciAt.Left > pcbBirinciAt.Left && pcbIkinciAt.Left > pcbUcuncuAt.Left)
+            {
+                this.Text = "Kara Şimşek önde götürüyor...";
+            }
+            else if (pcbUcuncuAt.Left > pcbBirinciAt.Left && pcbUcuncuAt.Left > pcbIkinciAt.Left)
+            {
+                this.Text = "Şahbatur önde götürüyor...";
+            }
+            
+            if (pcbBirinciAt.Left + pcbBirinciAt.Width >= lblFinish.Left)
+            {
+                timer1.Stop();
+                MessageBox.Show("Yarışı Rüzgar Kazandı !!!");
+                pcbBirinciAt.Enabled = pcbIkinciAt.Enabled = pcbUcuncuAt.Enabled = false;
+            }
+            else if (pcbIkinciAt.Left + pcbIkinciAt.Width >= lblFinish.Left)
+            {
+                timer1.Stop();
+                MessageBox.Show("Yarışı Kara Şimşek Kazandı !!!");
+                pcbBirinciAt.Enabled = pcbIkinciAt.Enabled = pcbUcuncuAt.Enabled = false;
+            }
+            else if (pcbUcuncuAt.Left + pcbUcuncuAt.Width >= lblFinish.Left)
+            {
+                timer1.Stop();
+                MessageBox.Show("Yarışı Şahbatur Kazandı !!!");
+                pcbBirinciAt.Enabled = pcbIkinciAt.Enabled = pcbUcuncuAt.Enabled = false;
+            }
+        }
+
+        private void btnYarisiSifirla_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            pcbBirinciAt.Left = 10;
+            pcbIkinciAt.Left = 10;
+            pcbUcuncuAt.Left = 10;
+            this.Text = "";
+            pcbBirinciAt.Enabled = pcbIkinciAt.Enabled = pcbUcuncuAt.Enabled = true;
+        }
+
+        private void btnRuzgarGaz_Click(object sender, EventArgs e)
+        {
+            if(timer1.Enabled)
+            pcbBirinciAt.Left += 15;
+        }
+
+        private void btnKaraSimsekGaz_Click(object sender, EventArgs e)
+        {
+            if(timer1.Enabled)
+            pcbIkinciAt.Left += 15;
+        }
+
+        private void btnSahbaturGaz_Click(object sender, EventArgs e)
+        {
+            if(timer1.Enabled)
+            pcbUcuncuAt.Left += 15;
+        }
+    }
+}
